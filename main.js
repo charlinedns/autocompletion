e0cb9c95c510a876fc1c4a88ba2a8511746629df
Charline <denis.charline@outlook.fr>
2020-04-03 10:51:53 +0200

    *Ajout d'un fichier css *Ajout de la recherche par filme     -> A CORRIGER!

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-    const cast=document.getElementById("year");
-    const castReturn=document.getElementById("yearReturn");
+    const year=document.getElementById("year");
+    const yearReturn=document.getElementById("yearReturn");
 
-    cast.addEventListener("keyup",function(){
+    year.addEventListener("keyup",function(){
         let tmpYear = year.value;
             yearReturn.innerHTML=tmpYear;
             //Appeler la page php
             //Ajout avec {} la méthode utilisée dans form, l'encodage et les variables que l'on veut envoyer à cette page
-            fetch("cast.php",{
+           fetch("year.php",{
                 method:"POST",
                 headers:{"Content-Type":'application/x-www-form-urlencoded'},
                 body:"tmpYear="+tmpYear
@@ -18,9 +18,25 @@ document.addEventListener('DOMContentLoaded', function(){
                 //console.log(text);
                 yearReturn.innerHTML=text;
             })
+    })
 
+    const cast=document.getElementById("cast");
+    const returnCast=document.getElementById("returnCast");
 
-
+    cast.addEventListener("keyup", function(){
+        let tmpCast=cast.value;
+        //console.log("debug");
+          returnCast.innerHTML=tmpCast;
+          fetch("year.php",{
+                method:"POST",
+                headers: {"Content-Type":'application/x-www-form-urlencoded'},
+                body:"tmpCast="+tmpCast
+            }).then(function(response){
+                return response.text();
+            }).then(function(text){
+                //console.log(text);
+                returnCast.innerHTML=text;
+            })
     })
 
 
--- year.php
+++ year.php
@@ -5,17 +5,31 @@ try {
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
     ));
-}
-catch (PDOException $e) {
+} catch (PDOException $e) {
     echo 'Erreur de connexion : ' . $e->getMessage();
 }
 
 //Récupérer la saisie de l'utilisateur
-$tmpYear="%".$_POST['tmpYear']."%";
 
-$rq = $pdo->prepare("SELECT DISTINCT(year) FROM movies_full WHERE year LIKE ? ORDER BY year");
-$rq->execute([$tmpYear]);
-    while($result = $rq->fetch(PDO::FETCH_ASSOC)){
-        echo "<div>".$result['year']."</div>";
+    $tmpYear = "%" . $_POST['tmpYear'] . "%";
+    //Sélection de la date
+    $rq = $pdo->prepare("SELECT DISTINCT(year) FROM movies_full WHERE year LIKE ? ORDER BY year");
+    $rq->execute([$tmpYear]);
+    while ($result = $rq->fetch(PDO::FETCH_ASSOC)) {
+        echo "<div>" . $result['year'] . "</div>";
+    }
+
+
+
+
+
+//Sélection du titre
+
+    $tmpCast = "%" . $_POST['tmpCast'] . "%";
+
+    $rq = $pdo->prepare("SELECT DISTINCT (title) FROM movies_full WHERE title LIKE ? ORDER BY title");
+    $rq->execute([$tmpCast]);
+    while ($result = $rq->fetch(PDO::FETCH_ASSOC)) {
+        echo "<div>" . $result['title'] . "</div>";
     }
 
